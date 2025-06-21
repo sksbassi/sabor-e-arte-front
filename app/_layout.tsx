ef720e9a5dc3a304164dae2e8c9542bb74a87ebe
Joao Pires <joaovictor.2005.pires@gmail.com>
2025-06-21 10:32:58 -0300

    Deletada a pasta meuApp criado por engano na geração do apk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 import { AuthProvider } from "../src/contexts/authContext";
 
 
-export default function RootLayout() {
+export default function RootLayout() { //layout da aplicação, executado antes de qualquer tela
   return (
     <AuthProvider>
       <Stack
